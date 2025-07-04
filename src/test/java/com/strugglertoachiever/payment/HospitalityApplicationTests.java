ac239c7d35eea3c2de5cc228f44a03624cbf369d
vishawanath <vishu.bengaluru@gmail.com>
2025-07-05 01:01:53 +0530

    Initial commit

---
+++ src/test/java/com/strugglertoachiever/payment/HospitalityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strugglertoachiever.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
